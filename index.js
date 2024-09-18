769bdf27dcde73a464d81cf38e963fb7cdc8b484
Yedidya Newlander <ydidya.n@gmail.com>
2024-09-18 15:20:46 +0300

    startup a express server

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+// import * as express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+//middleware - function that is called when a request is made
+
+const changePoweredBy = (req, res, next) => {
+  const power = res.getHeader("x-powered-by");
+  if (power === "Express") {
+    res.setHeader("x-powered-by", "My Docker Image");
+  }
+  next();
+};
+
+app.use(changePoweredBy);
+
+app.use("/", (req, res) => {
+  const url = req.url;
+  const power = res.getHeader("x-powered-by");
+  const response = {
+    status: 200,
+    message: "Welcome to my API",
+    data: {
+      url: url,
+      "x-powered-by": power,
+    },
+  };
+  res.json(response);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
